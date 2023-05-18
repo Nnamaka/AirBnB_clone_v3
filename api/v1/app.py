800d36c62e68bb06843b2e67ce8543c3b7ed9e32
Nnamaka <nnamaka7@gmail.com>
2023-05-18 04:25:43 -0700

    task 5

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
 """main file """
-from flask import Flask
+from flask import Flask, jsonify, make_response
 import os
 from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """ handler returns 404 in json format """
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
